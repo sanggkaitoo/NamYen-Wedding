bfa7f24e4c6450fecc9fc55771f6c3316505933b
sanggkaitoo <pmsang.hust@gmail.com>
2023-04-18 11:04:01 +0700

    Submit and write file

--- writefile.php
+++ writefile.php
@@ -4,10 +4,9 @@ $name = $_REQUEST['name'];
 $which = $_REQUEST['which'];
 $guest = $_REQUEST['guest'];
 
+$string = $name . " " . $which . " " . $guest
+
 $file = fopen("submit.txt", "a");
-fwrite($file, "$name");
-fwrite($file, "$which");
-fwrite($file, "$guest");
-fwrite($file, "============");
+fwrite($file, "$string");
 fclose($file);
 ?>
